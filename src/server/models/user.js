e6ccb2e8201f4b4d09548b97f70e7c2e133d3685
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-03 20:49:38 +0200

    Add authId, email and created fields in User.

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 
 var User = mongoose.model('User', {
+  authId: {type: String, unique: true},
   username: {type: String, unique: true},
   password: String,
+  email: {type: String, unique: true},
+  created: Date
 });
 
 module.exports = User;
