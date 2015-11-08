b0be78556741d6021df7fffa5b0978f940b2c0de
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-08 18:10:53 +0200

    Set stickyStateProvider.enableDebug, set locationProvider.html5Mode.

--- src/public/app/config.js
+++ src/public/app/config.js
@@ -1,7 +1,9 @@
 var app = require('angular').module('app');
 
-app.config(function($logProvider) {
+app.config(function($locationProvider, $logProvider, $stickyStateProvider) {
 
   $logProvider.debugEnabled(true);
+  $stickyStateProvider.enableDebug(true);
+  $locationProvider.html5Mode(true);
 
 });
