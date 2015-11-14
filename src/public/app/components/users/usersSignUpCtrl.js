514aa4436a001817ef06eeca15d5183d821bf968
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-14 20:27:06 +0200

    Validate User using mongoose.Document.

--- src/public/app/components/users/usersSignUpCtrl.js
+++ src/public/app/components/users/usersSignUpCtrl.js
@@ -1,18 +1,28 @@
 var app = require('angular').module('app');
+var userSchema = require('models/userSchema');
 
-app.controller('usersSignUpCtrl', function($log, $scope, User) {
+app.controller('usersSignUpCtrl', function($log, $scope, $state, User) {
 
   'use strict';
 
   $scope.users = {};
-  $scope.users.added = new User();
+  $scope.users.added = mongoose.Document({}, userSchema);
 
   $scope.users.signUp = function() {
     $log.debug('New user is signing up', $scope.users.added);
-    $scope.users.added.$signUp(function(newUser) {
-      $log.debug('New user is', newUser);
-
+    $scope.users.added.validate(function(err) {
+      if (err) {
+        $log.debug('Validation error when adding user', err.errors);
+        return;
+      }
+      var data = {};
+      $scope.users.added.displayFields().forEach(function(key) {
+        data[key] = $scope.users.added[key];
+      });
+      new User(data).$signUp(function(newUser) {
+        $log.debug('New user is', newUser);
+      });
+      $state.go('app.home');
     });
   };
-
 });
