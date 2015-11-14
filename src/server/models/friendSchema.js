93ce73f1f397c2127646a079b35c7757ce14a005
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-14 20:21:04 +0200

    In Mongoose models add method displayFields.

--- src/server/models/friendSchema.js
+++ src/server/models/friendSchema.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
-  name: {type: String, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
   age: {type: Number, required: true, min: 0, max: 100},
+  email: {type: String, required: true},
   address: {type: String, required: true},
 });
+
+schema.methods.displayFields = function() {
+  return ['firstName', 'lastName', 'age', 'email', 'address'];
+};
 
 module.exports = schema;
--- src/server/models/userSchema.js
+++ src/server/models/userSchema.js
@@ -4,19 +4,21 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
     created: Date
   });
 
-// generating a hash
 schema.methods.generateHash = function(password) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
-// checking if password is valid
 schema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.password);
+};
+
+schema.methods.displayFields = function() {
+  return ['firstName', 'lastName', 'email', 'password'];
 };
 
 module.exports = schema;
