1510b14cd6d3f323343de6e5a2caded098293b01
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-15 19:24:37 +0200

    Add email validation.

--- src/server/models/friendSchema.js
+++ src/server/models/friendSchema.js
@@ -1,10 +1,18 @@
 var mongoose = require('mongoose');
 
+var validateEmail = function(email) {
+  return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/.test(email);
+};
+
 var schema = new mongoose.Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
   age: {type: Number, required: true, min: 0, max: 100},
-  email: {type: String, required: true},
+  email: {
+    type: String,
+    required: true,
+    validate: [validateEmail, 'Please fill a valid email address.']
+  },
   address: {type: String, required: true},
 });
 
