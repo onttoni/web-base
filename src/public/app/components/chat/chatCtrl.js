03bf0e9e5b122488b2237f128c2240e8dfb8dcf4
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-19 20:25:40 +0200

    Implement simple socket.io based chat.

--- src/public/app/components/chat/chatCtrl.js
+++ src/public/app/components/chat/chatCtrl.js
@@ -5,10 +5,27 @@ app.controller('chatCtrl', function($log, $scope, $state) {
 
   'use strict';
 
-  var socket = io.connect('http://localhost');
-  socket.on('news', function(data) {
-    console.log(data);
-    socket.emit('my other event', {my: 'data'});
+  $scope.chat = {};
+  $scope.chat.output = [];
+
+  var socket = io.connect('http://localhost:8080/chat');
+  socket.on('hello', function(data) {
+    $log.debug('socket:chat connected');
+    $scope.$apply(function() {
+      $scope.chat.output.push(data);
+    });
   });
+
+  socket.on('msg', function(data) {
+    $log.debug('socket:chat new message:', data);
+    $scope.$apply(function() {
+      $scope.chat.output.push(data);
+    });
+  });
+
+  $scope.chat.send = function() {
+    socket.emit('say', $scope.chat.input);
+    $scope.chat.input = '';
+  };
 
 });
