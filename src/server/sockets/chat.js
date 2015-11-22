1235319b6223107dd2d70306bb3e585c880a8f09
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-22 18:52:36 +0200

    Small refactoring.

--- src/server/sockets/chat.js
+++ src/server/sockets/chat.js
@@ -6,38 +6,41 @@ module.exports.events = function(socket) {
 
   'use strict';
 
+  function findUser(userId, callback) {
+    User.findOne({_id: userId}, callback);
+  }
+
+  function socketEmitBroadcast(event, data) {
+    socket.emit(event, data);
+    socket.broadcast.emit(event, data);
+  }
+
   socket.on('chat:join', function() {
-    User.findOne(
-      {_id: socket.request.session.passport.user},
+    findUser(socket.request.session.passport.user,
       function(err, user) {
         if (user) {
           var hello = moment().format('HH:mm:ss') + ' ' + user.email + ' joined';
-          socket.emit('chat:hello', hello);
-          socket.broadcast.emit('chat:hello', hello);
+          socketEmitBroadcast('chat:hello', hello);
         }
       });
   });
 
   socket.on('chat:msg', function(data) {
-    User.findOne(
-      {_id: socket.request.session.passport.user},
+    findUser(socket.request.session.passport.user,
       function(err, user) {
         if (user) {
           var msg = moment().format('HH:mm:ss') + ' ' + user.email + ' ' + data;
-          socket.emit('chat:say', msg);
-          socket.broadcast.emit('chat:say', msg);
+          socketEmitBroadcast('chat:say', msg);
         }
       });
   });
 
   socket.on('chat:leave', function() {
-    User.findOne(
-      {_id: socket.request.session.passport.user},
+    findUser(socket.request.session.passport.user,
       function(err, user) {
         if (user) {
           var bye = moment().format('HH:mm:ss') + ' ' + user.email + ' left';
-          socket.emit('chat:bye', bye);
-          socket.broadcast.emit('chat:bye', bye);
+          socketEmitBroadcast('chat:bye', bye);
         }
       });
   });
