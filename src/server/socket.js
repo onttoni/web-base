c944cbb8b805d15307bbc7e61073c127dbee7b19
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-22 13:09:54 +0200

    Fix socket io usage in server.

--- src/server/socket.js
+++ src/server/socket.js
@@ -4,13 +4,12 @@ var log = require('./logger');
 
 module.exports = function(server, session) {
 
-  var socketIo = require('socket.io')(server);
+  var socketIo = require('socket.io');
+  var io = socketIo.listen(server);
 
-  socketIo.use(function(socket, next) {
+  io.use(function(socket, next) {
     session(socket.request, {}, next);
   });
-
-  var io = socketIo.listen(server);
 
   io.use(function(socket, next) {
     var userId;
@@ -24,6 +23,7 @@ module.exports = function(server, session) {
   });
 
   io.on('connection', function(socket) {
+    log.debug('Connecting socket.');
     var sckDir = path.join(__dirname, 'sockets');
     log.debug('Scanning', sckDir, 'for socket event handlers.');
     fs.readdirSync(sckDir).forEach(function(file) {
@@ -32,7 +32,10 @@ module.exports = function(server, session) {
         require(path.join(sckDir, file)).events(socket);
       }
     });
+    socket.emit('connection:open');
+    socket.on('disconnect', function() {
+      log.debug('Disconnecting socket.');
+    });
   });
-
   return io;
 };
