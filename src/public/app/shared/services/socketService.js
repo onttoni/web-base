e259b5cc67be1f185c388a6d5d9ecf024e7b3d2b
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-11-22 14:57:29 +0200

    Improve handling of connection events.

--- src/public/app/shared/services/socketService.js
+++ src/public/app/shared/services/socketService.js
@@ -1,17 +1,38 @@
 var app = require('angular').module('app');
 
-app.service('SocketService', function() {
-  var io = require('socket.io-client');
-  var socket = io.connect(window.location.origin);
+app.service('SocketService', function($log, $rootScope) {
 
-  this.on = function(eventName, callback) {
-    socket.on(eventName, function() {
-      var args = arguments;
-      callback.apply(socket, args);
-    });
+  'use strict';
+
+  var io = require('socket.io-client');
+  var socket = null;
+  var connectionOptions =  {
+    'forceNew': true,
+    'reconnection': true,
+    'reconnectionDelay': 2000,
+    'reconnectionDelayMax': 60000,
+    'reconnectionAttempts': 'Infinity',
+    'timeout': 10000
+  };
+
+  connect();
+
+  this.connect = function() {
+    connect();
+  };
+
+  this.disconnect = function() {
+    disconnect();
+  };
+
+  this.isConnected = function() {
+    return socket !== null;
   };
 
   this.emit = function(eventName, data, callback) {
+    if (!socket) {
+      return;
+    }
     socket.emit(eventName, data, function() {
       var args = arguments;
       if (callback) {
@@ -19,4 +40,47 @@ app.service('SocketService', function() {
       }
     });
   };
+
+  this.on = function(eventName, callback) {
+    if (!socket) {
+      return;
+    }
+    socket.on(eventName, function() {
+      var args = arguments;
+      callback.apply(socket, args);
+    });
+  };
+
+  function connect() {
+    if (!socket) {
+      $log.debug('SocketService connecting');
+      socket = io.connect(window.location.origin, connectionOptions);
+      socket.on('connection:open', function() {
+        $log.debug('SocketService connected');
+      });
+      socket.on('connection:unauthorized', function() {
+        $log.debug('SocketService connection unauthorized');
+        disconnect();
+      });
+    }
+  }
+
+  function disconnect() {
+    if (socket) {
+      $log.debug('SocketService disconnecting');
+      socket.disconnect();
+      socket = null;
+    }
+  }
+
+  $rootScope.$on('user:signIn', function() {
+    $log.debug('SocketService <- user:signIn');
+    connect();
+  });
+
+  $rootScope.$on('user:signOut', function() {
+    $log.debug('SocketService <- user:signOut');
+    disconnect();
+  });
+
 });
