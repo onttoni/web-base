b3612f42b7f05ad7c5fef19c8ef52812699e8b61
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-02 16:33:06 +0200

    Add config for expressSession and jsonwebtoken.

--- src/server/config.js
+++ src/server/config.js
@@ -4,6 +4,13 @@ module.exports = cfg;
 cfg.express = {};
 cfg.express.httpPort = 8080;
 
+cfg.expressSession = {};
+cfg.expressSession.secret = 'foobar';
+
+cfg.jsonwebtoken = {};
+cfg.jsonwebtoken.secret = 'foobar';
+cfg.jsonwebtoken.expires = 60 * 24;
+
 cfg.mongo = {};
 cfg.mongo.host = 'mongo01.local';
 cfg.mongo.port = 27017;
