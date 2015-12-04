20a4e202765ba57227111b133162cf28bbdc2ee6
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-04 19:25:50 +0200

    Prevent traceback in http 401 response.

--- src/server/controllers/authenticationController.js
+++ src/server/controllers/authenticationController.js
@@ -2,8 +2,14 @@ var expressJwt = require('express-jwt');
 
 module.exports.controller = function(app, apiPrefix) {
 
-  app.all(apiPrefix + 'friends/', expressJwt({
+  app.all(apiPrefix + 'friends/',
+  expressJwt({
     secret: require('../config').jsonwebtoken.secret
-  }));
-
+  }),
+  function(err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+      return res.status(401).send({msg: 'unauthorized'});
+    }
+    next();
+  });
 };
