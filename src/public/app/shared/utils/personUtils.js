48d99a9edabb568b7aed945d82b328c172145c88
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-05 19:41:05 +0200

    Move friendsUtils to personUtils.

---
+++ src/public/app/shared/utils/personUtils.js
@@ -0,0 +1,22 @@
+var _ = require('lodash');
+var mongoose = require('mongoose');
+var friendSchema = require('models/friendSchema');
+var userSchema = require('models/userSchema');
+
+module.exports = {
+  getPersonDoc: function(scope, data, schemaName) {
+    var schema = schemaName === 'friendSchema' ? friendSchema : userSchema;
+    scope.personDoc = mongoose.Document(data, schema);
+    scope.displayFields = {};
+    scope.personDoc.displayFields().forEach(function(field) {
+      scope.displayFields[field] = 'personDoc.' + field;
+    });
+  },
+  extractDocData: function(scope) {
+    var data = {};
+    _.keys(scope.displayFields).forEach(function(field) {
+      data[field] = _.get(scope, scope.displayFields[field]);
+    });
+    return data;
+  }
+};
