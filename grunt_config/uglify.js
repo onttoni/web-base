c63c7eafa1df43a27266cd617c9764ba4173af6b
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-13 14:27:38 +0200

    Uglify all files in build/public and add screwIE8, drop_console.

--- grunt_config/uglify.js
+++ grunt_config/uglify.js
@@ -1,7 +1,10 @@
 module.exports = {
   options: {
-    banner: '/*! <%= package.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    banner: '/*! <%= package.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+    compress: {drop_console: true},
+    screwIE8: true
   },
+  /*
   vendor: {
     files: {
       'build/public/app/vendor.js': ['build/public/app/vendor.js']
@@ -11,5 +14,14 @@ module.exports = {
     files: {
       'build/public/app/app.js': ['build/public/app/app.js']
     }
+  }
+  */
+  dist: {
+    files: [{
+      expand: true,
+      cwd: 'build/public',
+      src: '**/*.js',
+      dest: 'build/public'
+    }]
   }
 };
