7b1b4e155b8fcc9baff66aee86a525f604e79583
Mikko Karvonen <mikko.karvonen@ymail.com>
2015-12-13 14:33:32 +0200

    Set debug for ocLazyLoadProvider.

--- src/public/app/config.js
+++ src/public/app/config.js
@@ -1,9 +1,14 @@
-var app = require('angular').module('app');
+define(['angular'], function(angular) {
 
-app.config(function($locationProvider, $logProvider, $stickyStateProvider) {
+  var app = angular.module('app');
 
-  $logProvider.debugEnabled(true);
-  $stickyStateProvider.enableDebug(true);
-  $locationProvider.html5Mode(true);
+  app.config(function($locationProvider, $logProvider, $ocLazyLoadProvider, $stickyStateProvider) {
+
+    $logProvider.debugEnabled(true);
+    $stickyStateProvider.enableDebug(true);
+    $locationProvider.html5Mode(true);
+    $ocLazyLoadProvider.config({debug: true});
+
+  });
 
 });
